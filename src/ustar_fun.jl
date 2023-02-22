1b39ab6bcc2c867dce26322d5fd8f89ab278dda2
Iván Esteban Gutiérrez Martínez <iegutierrezm@ine.gob.cl>
2023-02-22 17:02:32 -0300

    Improves the implementation of `ustar_fun()`

--- src/ustar_fun.jl
+++ src/ustar_fun.jl
@@ -1,12 +1,21 @@
-# Approximate u*(x) := t(x) - z^2 / 2 given x
+# In the notation of [1]:
+# Return u*(x) := t(x) - z^2 / 2, given x.
 function ustar_fun(x::Real)
     ustar0 = init_ustar(x)
-    f(u) = K_prime1_fun(u) - x
-    g(u) = K_prime2_fun(u)
-    newtons_method(f, g, ustar0)
+    gh(u) = target_gh(u, x)
+    newtons_method(gh, ustar0)
 end
 
-# Compute a numerically stable approximation of K'(t), given z
+# In the notation of [1]:
+# Return the first two derivatives of the target K(s) - s * x (wrt u).
+function target_gh(u::Real, x::Real)
+    gu = K_prime1_fun(u) - x
+    hu = K_prime2_fun(u, gu + x)
+    (gu, hu)
+end
+
+# In the notation of [1]:
+# Return the 1st derivative of K(t) (wrt u)
 # Note: The approximation around 0 comes from the BayesLogit (R) package
 function K_prime1_fun(u::Real)
     s = 2 * u
@@ -22,10 +31,10 @@ function K_prime1_fun(u::Real)
     end
 end
 
-# Compute a numerically stable approximation of K''(t), given z
+# In the notation of [1]:
+# Return the 2nd derivative of K(t) (wrt u)
 # Note: The Taylor expansion around 0 comes from the BayesLogit (R) package
-function K_prime2_fun(u)
-    K_prime1 = K_prime1_fun(u)
+function K_prime2_fun(u, K_prime1)
     s = 2 * u
     if abs(s) > sqrt(eps(typeof(s)))
         return K_prime1^2 + (1 - K_prime1) / s
@@ -35,19 +44,7 @@ function K_prime2_fun(u)
     end
 end
 
-# Find a root of f(x) using Newton's method (adapted from [2, p. 91])
-function newtons_method(f, g, x; x_tol = 1e-8, iterations = 1_000)
-    dx = Inf
-    iter = 1
-    noise = g(x) |> typeof |> eps |> sqrt
-    while abs(dx) > x_tol && iter <= iterations
-        dx = f(x) / (g(x) + noise)
-        iter += 1
-        x -= dx
-    end
-    return x
-end
-
+# In the notation of [1]:
 # Return a starting guess for u*(x) = t(x) - z^2 / 2
 # Note: The guesses were taken from the (Python) package polyagamma,
 # see https://github.com/zoj613/polyagamma/blob/main/src/pgm_saddle.c#L119
@@ -61,64 +58,23 @@ function init_ustar(x)
     1.15
 end
 
+# Find a root of f(x) using Newton's method, given an initial value x
+# and a function `fg()` such that fg(x) returns f(x) and g(x) := f'(x).
+# Note: This function was adapted from [2, p.91]
+function newtons_method(fg, x; x_tol = 1e-8, iterations = 100)
+    dx = Inf
+    iter = 1
+    fx, gx = fg(x)
+    noise = gx |> typeof |> eps |> sqrt
+    while abs(dx) > x_tol && iter <= iterations
+        fx, gx = fg(x)
+        dx = fx / (gx + noise)
+        iter += 1
+        x -= dx
+    end
+    return x
+end
+
 # tstar_fun(0.5, 1.0) # tstar(0.5, 1.0) ≈ -1.33, entonces
 # tstar_fun(0.5, 1.0) - 1.0^2 / 2 # ustar(0.5) debería ser -1.83
-ustar_fun(0.5)
-
-#region TODO
-# - Improve get_initial_tstar().
-#endregion
-
-#region References
-# [1]
-# https://arxiv.org/abs/1405.0506
-#endregion
-
-#region trash
-# # Compute t*(x) := \text{armgin}_s Kz(s) - s * x using bisection
-# # Note: this is called t(x) in [1]
-# function t_star_fun2(x::Real, z::Real)
-#     f(t) = K_prime1_fun(t, z) - x
-#     a = -16.0
-#     b = +16.0
-#     bisection(f, a, b)
-# end
-
-# # Find a root of f(x) in [a, b] (taken from [2, p.50])
-# function bisection(f, a, b; x_tol = 1e-8, iterations = 1_000)
-#     ya = f(a)
-#     yb = f(b)
-#     ya == 0 && (b = a)
-#     yb == 0 && (a = b)
-#     iter = 1
-#     while b - a > x_tol || iter <= iterations
-#         x = (a + b) / 2
-#         y = f(x)
-#         if y == 0
-#             a, b = x, x
-#         elseif sign(y) == sign(ya)
-#             a = x
-#         else
-#             b = x
-#         end
-#         iter += 1
-#     end
-#     println(iter)
-#     return (a + b) / 2
-# end
-
-# # Expand interval = [a, b] until f(a) * f(b) < 0 (taken from [2, p.50])
-# function bracket_sign_change!(f, interval; k = 2)
-#     a, b = interval
-#     center = (b + a) / 2
-#     half_width = (b - a) / 2
-#     while f(a) * f(b) > 0
-#         half_width *= k
-#         a = center - half_width
-#         b = center + half_width
-#     end
-#     interval[1] = a
-#     interval[2] = b
-#     return interval
-# end
-#endregion
+@time ustar_fun(0.5);
