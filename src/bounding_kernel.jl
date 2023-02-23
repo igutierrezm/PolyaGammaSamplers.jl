880d6eff2d2dae315820ffc5514ffa9d26674d1a
Iván Esteban Gutiérrez Martínez <iegutierrezm@ine.gob.cl>
2023-02-23 09:54:42 -0300

    Fix bug in `bounding_kernel()`

--- src/bounding_kernel.jl
+++ src/bounding_kernel.jl
@@ -1,6 +1,6 @@
-function bounding_kernel_lf(x, z, xc, ustar, n)
-    intercept_lf, slope_lf = tangent_to_eta(x, z, xc, ustar)
-    alpha_lf = K_prime2_star_fun(x, ustar) / xc^3
+function bounding_kernel_lf(x, z, xc, ustarc, n)
+    intercept_lf, slope_lf = tangent_to_eta(x, z, xc, ustarc)
+    alpha_lf = K_prime2_star_fun(x, ustarc) / xc^3
     log_out = (
         log(n / 2 / pi) / 2 -
         log(alpha_lf) / 2 +
@@ -12,9 +12,9 @@ function bounding_kernel_lf(x, z, xc, ustar, n)
     exp(log_out)
 end
 
-function bounding_kernel_rg(x, z, xc, ustar, n)
-    intercept_rg, slope_rg = tangent_to_eta(x, z, xc, ustar)
-    alpha_rg = K_prime2_star_fun(x, ustar) / xc^2
+function bounding_kernel_rg(x, z, xc, ustarc, n)
+    intercept_rg, slope_rg = tangent_to_eta(x, z, xc, ustarc)
+    alpha_rg = K_prime2_star_fun(x, ustarc) / xc^2
     log_out = (
         log(n / 2 / pi) / 2 -
         log(alpha_rg) / 2 +
